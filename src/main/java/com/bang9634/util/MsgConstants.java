2cf6ef2bae56c7674add4819ad972c2be156e9cc
Bang9634 <ejrwo9634@gmail.com>
2025-07-16 09:19:36 +0900

    feat: Add MsgConstants class for replacing hardcoded msg

---
+++ src/main/java/com/bang9634/util/MsgConstants.java
@@ -0,0 +1,17 @@
+package com.bang9634.util;
+
+public class MsgConstants {
+    public static final String TITLE = "Weather Forecast";
+    public static final String NOTICE = "알림";
+    public static final String ERROR = "오류";
+
+    public static final String BUTTON_INIT_SERVICE_KEY = "서비스 키 초기화";
+    public static final String BUTTON_ACCEPT = "확인";
+
+    public static final String MSG_DIALOG_INIT_SERVICE_KEY = "서비스 키가 초기화되었습니다.\n초기화면으로 돌아갑니다.";
+    public static final String MSG_INPUT_SERVICE_KEY = "서비스 키를 입력하세요.";
+    public static final String MSG_AUTH_SUCCESS = "인증 성공";
+    public static final String MSG_AUTH_FAIL = "인증 실패";
+
+    
+}
